7567781cf4029a998e5eb8d4d81dac1bc749ecbb
usman <muhammad.usma@techverx.com>
2022-03-16 16:01:13 +0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root "pages#homepage"
+  get "about", to: "pages#about"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
