8e384843a82e700fc21e1b45b94981ed4f7b5431
usman <muhammad.usma@techverx.com>
2022-03-18 16:46:23 +0500

    Add edit, show, update and index page listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,36 @@
 class ArticlesController < ApplicationController
 
+
+    def index
+        @all_articles = Article.all
+
+    end
+
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+
+        else
+            render :edit, status: :unprocessable_entity
+
+        end
+
     end
 
     def create
